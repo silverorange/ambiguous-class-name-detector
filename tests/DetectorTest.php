364564beb5672c3663fd9970a34cefde5b27341c
Michael Gauthier <mike@silverorange.com>
2019-08-26 12:23:45 -0300

    Add detector tests

---
+++ tests/DetectorTest.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Silverorange\AmbiguousClassNameDetector;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @package   AmbiguousClassNameDetector
+ * @author    Michael Gauthier <mike@silverorange.com>
+ * @copyright 2019 silverorange
+ * @license   http://www.opensource.org/licenses/mit-license.html MIT License
+ */
+final class DetectorTest extends TestCase
+{
+    public function testReturnsEmptyArrayOnEmptyOutput()
+    {
+        $detector = new Detector();
+        $this->assertEquals([], $detector->getAmbiguousClassNames(''));
+    }
+
+    public function testReturnsEmptyArrayOnNonMatchingOutput()
+    {
+        $detector = new Detector();
+        $this->assertEquals(
+            [],
+            $detector->getAmbiguousClassNames(
+                "Generated autoload files containing 574 classes\n"
+            )
+        );
+    }
+
+    public function testReturnsSingleMatch()
+    {
+        $detector = new Detector();
+        $this->assertEquals(
+            [
+                [
+                    'class_name' => 'Application',
+                    'file1' => '/foo/bar/include/Application.php',
+                    'file2' => '/foo/bar/include/Application2.php'
+                ]
+            ],
+            $detector->getAmbiguousClassNames(
+                'Generating autoload filesWarning: Ambiguous class ' .
+                    "resolution, \"Application\" was found in both " .
+                    "\"/foo/bar/include/Application.php\" and " .
+                    "\"/foo/bar/include/Application2.php\", the first will " .
+                    "be used.\n " .
+                    "Generated autoload files containing 2906 classes\n"
+            )
+        );
+    }
+
+    public function testReturnsMultipleMatches()
+    {
+        $detector = new Detector();
+        $this->assertEquals(
+            [
+                [
+                    'class_name' => 'Application',
+                    'file1' => '/foo/bar/include/Application.php',
+                    'file2' => '/foo/bar/include/Application2.php'
+                ],
+                [
+                    'class_name' => 'Application',
+                    'file1' => '/foo/bar/include/Application.php',
+                    'file2' => '/foo/bar/include/Application3.php'
+                ]
+            ],
+            $detector->getAmbiguousClassNames(
+                'Generating autoload filesWarning: Ambiguous class ' .
+                    "resolution, \"Application\" was found in both " .
+                    "\"/foo/bar/include/Application.php\" and " .
+                    "\"/foo/bar/include/Application2.php\", the first will " .
+                    "be used.\n " .
+                    "Warning: Ambiguous class resolution, \"Application\" " .
+                    "was found in both \"/foo/bar/include/Application.php\" " .
+                    "and \"/foo/bar/include/Application3.php\", the first " .
+                    "will be used.\n" .
+                    "Generated autoload files containing 2906 classes\n"
+            )
+        );
+    }
+}
