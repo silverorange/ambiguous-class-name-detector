40f0e33964d4b1ba6d0b4bd01e74330a901a100a
Colin Viebrock <colin@viebrock.ca>
2026-06-24 17:42:58 -0400

    Convert to module

--- prettier.config.js
+++ prettier.config.js
@@ -2,7 +2,7 @@
  * @see https://prettier.io/docs/en/configuration.html
  * @type {import("prettier").Config}
  */
-module.exports = {
+const config = {
   singleQuote: true,
   tabWidth: 2,
   trailingComma: 'none',
@@ -18,3 +18,5 @@ module.exports = {
     }
   ]
 };
+
+export default config;
